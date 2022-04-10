fd196021f2b1bbdb5c37aba6ac7270f9325012b1
Marius Albeser <68159866+Sletger@users.noreply.github.com>
2022-04-10 14:40:12 +0200

    basic app setup

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { App }  from "./app/App"
+
+const entryPoint = document.getElementById("entry-point");
+
+ReactDOM.render(
+    <App/>,entryPoint
+);
