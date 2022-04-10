b3bde11d1d0b297e6e41f855b8fb8010e415580f
Marius Albeser <68159866+Sletger@users.noreply.github.com>
2022-04-10 15:43:55 +0200

    Mulit-Select mit EventListener values und onChange props angepasst -> onChange property an .this gebindet

--- src/app/RegisterForm.jsx
+++ src/app/RegisterForm.jsx
@@ -4,10 +4,12 @@ export class RegisterForm extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            name : ""
+            name : "",
+            tShirtSize : "w-s"
         };
 
         this.handleChange = this.handleChange.bind(this); // set und this Stolperfall -> andere Schreibweise property in constructor an Instanz binden sodass immer das "richtige" this ausgewählt wird
+        this.handleTshirtSizeChange = this.handleTshirtSizeChange.bind(this);
     }
 
     handleChange (event) {
@@ -16,6 +18,12 @@ export class RegisterForm extends React.Component {
         })
     }
 
+    handleTshirtSizeChange (event) {
+        this.setState({
+            tShirtSize : event.target.value
+        })
+    }
+    
     render() {
         return (
             <form>
@@ -23,8 +31,20 @@ export class RegisterForm extends React.Component {
                     value={this.state.name} //Kontrolle von React App -> bedeutet auch das ohne onChange Listener kann default Text noch nicth verändert werden
                     onChange={this.handleChange}
                 />
+                <br></br>
+                <br></br>
+                <select
+                    value={this.state.tShirtSize}
+                    onChange={this.handleTshirtSizeChange}
+                >                   
+                    <option value="w-s">W-S</option> 
+                    <option value="w-m">W-M</option> 
+                    <option value="w-l">W-L</option> 
+                    <option value="m-s">M-S</option> 
+                    <option value="m-m">M-M</option> 
+                    <option value="m-l">M-L</option>
+                </select>
             </form>
-
         )
     }
 
