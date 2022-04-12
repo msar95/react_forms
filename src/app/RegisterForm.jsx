0fff8d141dbf042ba86d2c8959ea22ab815135d2
Marius Albeser <68159866+Sletger@users.noreply.github.com>
2022-04-12 10:41:03 +0200

    Regex Check Name Input

--- src/app/RegisterForm.jsx
+++ src/app/RegisterForm.jsx
@@ -4,17 +4,23 @@ import { LunchInput } from "./controlled components/LunchInput";
 import { NameInput } from "./controlled components/NameInput";
 import { SizeInput } from "./controlled components/SizeInput";
 
+const emptyForm = {
+    name: "",
+    tShirtSize: "w-l",
+    lunch: false,
+    formSubmitted: false,
+    nameError: false
+}
+
 export class RegisterForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            name: "",
-            tShirtSize: "w-l",
-            lunch: false,
-            formSubmitted: false
-        };
+        this.state = emptyForm;
+
         this.handleChange = this.handleChange.bind(this); // set und this Stolperfalle -> andere Schreibweise property in constructor an Instanz binden sodass immer das "richtige" this ausgewählt wird
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleReset = this.handleReset.bind(this);
+        this.handleBlur = this.handleBlur.bind(this);
     }
 
     handleChange(event) {
@@ -24,21 +30,45 @@ export class RegisterForm extends React.Component {
         })
     }
 
-    handleSubmit() {
-        console.log(this.state);
-        this.setState({
-            formSubmitted : true
-        })
+    validateName() {
+        const regex = /[a-zA-Z]{3,}/;
+        const check = (regex.test(this.state.name)) ? true : false;
+        return check;
     }
 
+    handleBlur() {
+        if (!this.validateName()) {
+            this.setState({
+                nameError:true,
+                style: { backgroundColor: "red" }
+            })
+        } else {
+            this.setState({
+                nameError:false,
+                style: { backgroundColor: "white" }
+            })
+        }
+    }
+
+    handleSubmit() {
+        this.setState({
+            formSubmitted: true
+        })
+    }
+    handleReset() {
+        this.setState(emptyForm);
+    }
 
     render() {
-        if (! this.state.formSubmitted) {
+        if (!this.state.formSubmitted) {
             return (
                 <div>
                     <NameInput
                         value={this.state.name}
-                        onChange={this.handleChange} />
+                        onChange={this.handleChange}
+                        onBlur={this.handleBlur}
+                        style={this.state.style}
+                    />
                     <SizeInput
                         value={this.state.tShirtSize}
                         onChange={this.handleChange} />
@@ -49,14 +79,14 @@ export class RegisterForm extends React.Component {
                 </div>
             )
         } else {
-            return(
+            return (
                 <div>
-                    <h1>Du hast dich erfolgreich zum Seminar angemeldet!</h1>
+                    <h1>Du hast dich erfolgreich zum Event angemeldet!</h1>
+                    <button onClick={this.handleReset}>Zurücksetzen des Formulars</button>
                 </div>
             )
+
         }
 
-
     }
-
 }
--- src/app/controlled components/NameInput.jsx
+++ src/app/controlled components/NameInput.jsx
@@ -6,9 +6,10 @@ export class NameInput extends React.Component {
                 <label>Name:
                     <input type="text"
                         name="name"
-                        placeholder="Max Mustermann..."
                         value={this.props.value} //Kontrolle von React App -> bedeutet auch das ohne onChange Listener kann default Text noch nicth verändert werden
                         onChange={this.props.onChange}
+                        onBlur={this.props.onBlur}
+                        style ={this.props.style}
                     />
                 </label>
         )
