9c0bca7397a85abcbc4ab2ead00796470c4e001e
Marius Albeser <68159866+Sletger@users.noreply.github.com>
2022-04-18 13:25:22 +0200

    using state (activeTab) to controll which children gets rendered below the nav bar

--- src/app/wrapperComponents/TabController.jsx
+++ src/app/wrapperComponents/TabController.jsx
@@ -1,18 +1,30 @@
 import React from "react";
 
 export class TabController extends React.Component {
-    render () {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            activeTab: 0
+        }
+    }
+    render() {
         const tabSelection = this.props.children.map((tab, index) => {
-            return ( <li><a href="#"><b>{ tab.props.headline }</b></a></li>) //# bedeutet das auf der Seite geblieben wird, verhindert das Brwoser nach "link sucht"
-        })
+            return (
+                <li>
+                    <a href="#">
+                        <b>{tab.props.headline}</b>
+                    </a>
+                </li>) //# bedeutet das auf der Seite geblieben wird, verhindert das Brwoser nach "link sucht"
+        });
         return (
             <div>
                 <nav>
                     <ul>
-                       {tabSelection} 
+                        {tabSelection}
                     </ul>
                 </nav>
-                { this.props.children }
+                {this.props.children[this.state.activeTab]}
             </div>
         )
     }
